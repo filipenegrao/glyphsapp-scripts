0d570e89493b95a3de38aae143ab857791e534d2
filipenegrao <filipe@estudioanalogico.com.br>
2016-03-12 13:10:55 -0300

    Create exportAllOpenInstances.py
    File created.

---
+++ README.md
@@ -0,0 +1,18 @@
+##About
+
+####Export Open Instances 1.0
+This script creates a folder in your desktop and exports all open font instances there. It's important to say that **every time you run the script it will erase all files in the 'exported_fonts'** folder.
+
+
+##Installation
+Put the scripts into the *Scripts* folder which appears when you choose *Open Scripts Folder* from the *Scripts* menu.
+
+##Troubleshooting
+Please report any issues or pull requests here in GitHub. Don't forget to indicate your OS X and Glyphs App version.  
+
+##License
+Copyright (c) 2015 Filipe Negrão (@filipenegrao).
+
+Licensed under the MIT License (MIT). You may obtain a copy of this license at https://opensource.org/licenses/MIT
+
+See the License file included in this repository for further details.
---
+++ exportAllOpenInstances.py
@@ -0,0 +1,35 @@
+#MenuTitle: Export Open Instances 1.0
+# -*- coding: utf-8 -*-
+
+__doc__="""
+Export all open instances in OTF.
+"""
+
+import os, glob
+from os.path import expanduser
+
+# path to the folder where all files will be saved
+home = expanduser("~")
+desktop = '/Desktop/'
+folder = 'exported_fonts'
+path = home+desktop+folder
+
+# check if the folder already exists. If not, create the folder
+if os.path.exists(path):
+	print 'The %s already exists in %s' %(folder, path)
+else:
+	os.mkdir(path)
+	print 'Folder %s created in %s' %(folder, path)
+
+# remove all files before save the new ones
+all_files = glob.glob(path+'/*')
+for files in all_files:
+	os.remove(files)
+	 
+# check for all open fonts and export all instances to the created folder
+for font in Glyphs.fonts:
+	for instance in font.instances:
+		instance.generate(FontPath = path)
+	Glyphs.showNotification('Export fonts', 'The fonts were exported successfully.')
+
+	
