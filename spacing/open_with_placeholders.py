c72ae6dca6d3c4ded1733103f7e6aa10a19cdc87
Filipe Negrão <filipe@negrao.me>
2024-12-03 21:35:42 -0300

    removed Glyphs 2 / py2 support

--- spacing/open_with_placeholders.py
+++ spacing/open_with_placeholders.py
@@ -1,24 +1,21 @@
-#MenuTitle: Open selected glyphs with placeholders in a text tab
+# MenuTitle: Open selected glyphs with placeholders in a text tab
 # -*- coding: utf-8 -*-
 
-__doc__="""
+__doc__ = """
 Open a text tab with control characters (e.g., noHO) with all the glyphs selected in the Font View.
 """
 
-from __future__ import division, print_function, unicode_literals
-
-
 font = Glyphs.font
 
-lc_control = ['n', 'o']
-uc_control = ['H', 'O']
+lc_control = ["n", "o"]
+uc_control = ["H", "O"]
 
 selected = font.selection
-p = '/Placeholder '
+p = "/Placeholder "
 
-txt = 'nn{0}nnoo{0}oonn{0}oo\nHH{0}HHOO{0}OOHH{0}OO\n'.format(p)
+txt = "nn{0}nnoo{0}oonn{0}oo\nHH{0}HHOO{0}OOHH{0}OO\n".format(p)
 
 for g in selected:
-	txt += '/{}'.format(g.name)
-	
+    txt += "/{}".format(g.name)
+
 font.newTab(txt)
